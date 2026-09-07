24985b9e031e807312587e8eba102b03cc4fcf2e
Nikita Tsomuk <tsomuk@me.com>
2026-09-07 20:00:05 +0300

    Initial Commit

---
+++ ShoppingList38a/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShoppingList38a
+//
+//  Created by Nikita Tsomuk on 07.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShoppingList38a/ShoppingList38aApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingList38aApp.swift
+//  ShoppingList38a
+//
+//  Created by Nikita Tsomuk on 07.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingList38aApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
