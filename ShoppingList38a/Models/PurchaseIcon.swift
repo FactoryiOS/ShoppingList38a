49113aa5a144e60ebe39b6e07ea54c7f96b6dee5
Vadim Kislov <kislov.v@gmail.com>
2026-09-10 10:44:44 +0300

    [Feature] add icons to assets

---
+++ ShoppingList38a/Models/PurchaseIcon.swift
@@ -0,0 +1,31 @@
+//
+//  PurchaseIcon.swift
+//  ShoppingList38a
+//
+//  Created by Kislov Vadim on 10.09.2026.
+//
+
+import Foundation
+
+enum PurchaseIcon: String, CaseIterable {
+    case airplane = "airplaneOutline24"
+    case alert = "alertOutline24"
+    case balloon = "balloonOutline24"
+    case bandage = "bandageOutline24"
+    case barbell = "barbellOutline24"
+    case snow = "snowOutline24"
+    
+    case gift = "giftOutline24"
+    case calendarNumber = "calendarNumberOutline24"
+    case business = "businessOutline24"
+    case build = "buildOutline24"
+    case briefcase = "briefcaseOutline24"
+    case bed = "bedOutline24"
+    
+    case gameController = "gameControllerOutline24"
+    case paw = "pawOutline24"
+    case fastFood = "fastFoodOutline24"
+    case car = "carOutline24"
+    case cart = "cartOutline24"
+    case colorPalette = "colorPaletteOutline24"
+}
