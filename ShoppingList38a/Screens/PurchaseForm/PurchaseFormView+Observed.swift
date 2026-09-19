7d2ff7e83ce415e08b2ca8decab0573194647670
Vadim Kislov <kislov.v@gmail.com>
2026-09-19 17:20:28 +0300

    [Feature] add purchase form for create or edit

---
+++ ShoppingList38a/Screens/PurchaseForm/PurchaseFormView+Observed.swift
@@ -0,0 +1,64 @@
+//
+//  PurchaseFormView+Observed.swift
+//  ShoppingList38a
+//
+//  Created by Kislov Vadim on 19.09.2026.
+//
+
+import Foundation
+
+extension PurchaseFormView {
+    @MainActor
+    @Observable
+    final class Observed {
+        var name: String = ""
+        var selectedIcon: PurchaseIcon?
+        var selectedColor: PurchaseColor?
+        
+        private var currentPurchase: Purchase?
+        
+        private var trimmedName: String {
+            name.trimmingCharacters(in: .whitespacesAndNewlines)
+        }
+        
+        var isValid: Bool {
+            !trimmedName.isEmpty
+            && nameErrorMessage == nil
+            && selectedIcon != nil
+            && selectedColor != nil
+        }
+        
+        var submitButtonTitle: String {
+            currentPurchase != nil ? "Сохранить" : "Создать"
+        }
+        
+        var nameErrorMessage: String? {
+            if name.isEmpty || name == currentPurchase?.name {
+                return nil
+            }
+            
+            guard Purchase.mockPurchases.first(where: { $0.name == name }) != nil else {
+                return nil
+            }
+            
+            return "Это название уже используется, пожалуйста, измените его."
+        }
+        
+        func fetchPurchase(by id: UUID?) {
+            guard let id, let purchase = Purchase.mockPurchases.first(where: { $0.id == id }) else {
+                return
+            }
+            
+            name = purchase.name
+            selectedIcon = purchase.icon
+            selectedColor = purchase.color
+            
+            currentPurchase = purchase
+        }
+        
+        func savePurchase(with id: UUID?, completion: Completion) {
+            // TODO добавляем сохранение модели (добавляем или обновляем)
+            completion()
+        }
+    }
+}
